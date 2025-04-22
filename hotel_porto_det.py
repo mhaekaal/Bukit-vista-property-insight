823b6222711962ae0b7b03ff548752cdbb7680f1
mhaekaal <129404191+mhaekaal@users.noreply.github.com>
2025-04-22 13:22:57 +0700

    Update hotel_porto_det.py

--- hotel_porto_det.py
+++ hotel_porto_det.py
@@ -7,20 +7,12 @@ from closing import closing
 #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\inioc\streamlit-porto\bank-marketing-project-446413-a16e16990932.json'
 
 st.markdown('''<h1 style='text-align:center;font-size:40px;font-weight:bolder;color:#FF4B4B; line-height:2;'>
-            Haekal's Data Science Portfolio
+            Bukit Vista's Property Rental Estimator
          </h1>''', unsafe_allow_html=True)
-tab1, tab2 = st.tabs(['About Me', 'Bukit Vista Project'])
+tab1 = st.tabs(['Bukit Vista Project'])
 
 #About me
 with tab1:
-    aboutme()
-    st.markdown('         ')
-    st.markdown('         ')
-    st.markdown('         ')
-    st.markdown('         ')
-    closing()
-    
-with tab2:
     bv_project_det()
     st.markdown('         ')
     st.markdown('         ')
